63ef0fa05858295317595774924c92f9f1dfbfba
Wesley Wigham <wwigham@gmail.com>
2017-05-11 11:29:49 -0400

    Show more data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import { FlatButton } from "material-ui";
+import { FlatButton, Card, CardMedia, GridList, GridTile, CardText } from "material-ui";
 import { connect } from "react-redux";
 import { ModelState } from "glacier";
 
 export default connect(
-  (state: {glacier: ModelState, display: {viz: string, curAction: number}}) => ({
-    viz: state.display.viz
+  (state: {glacier: ModelState, display: {
+    viz: string, curAction: number, englishText: string, prettyJson: string
+  }}) => ({
+    viz: state.display.viz,
+    text: state.display.englishText,
+    json: state.display.prettyJson
   }),
   dispatch => ({
     onNextClick: () => dispatch({type: "ADVANCE_ACTION"})
@@ -14,8 +18,21 @@ export default connect(
 )(props => (
 <MuiThemeProvider>
   <div>
-    <FlatButton onClick={props.onNextClick}>Next</FlatButton>
-    <div dangerouslySetInnerHTML={{__html: props.viz}}/>
+    <FlatButton onClick={props.onNextClick}>Next</FlatButton><h3 style={{display: "inline-block"}}>{props.text}</h3>
+    <GridList cellHeight="auto">
+      <GridTile>
+    <Card>
+      <CardMedia>
+        <div dangerouslySetInnerHTML={{__html: props.viz}}/>
+      </CardMedia>
+    </Card>
+    </GridTile>
+    <GridTile>
+    <Card>
+        <CardText><pre><code>{props.json}</code></pre></CardText>
+    </Card>
+    </GridTile>
+    </GridList>
   </div>
 </MuiThemeProvider>
 ));
--- src/index.tsx
+++ src/index.tsx
@@ -15,17 +15,38 @@ injectTapEventPlugin();
 export interface DispState {
   viz: string;
   curAction: number;
+  englishText: string;
+  prettyJson: string;
 }
 
-const dispReducer = (state: DispState, action: {type: "UPDATE_VIZ_PREVIEW", payload: string}) => {
+const dispReducer = (
+  state: DispState,
+  action: {
+    type: "UPDATE_VIZ_PREVIEW", payload: string
+  } | {
+    type: "ADVANCE_ACTION"
+  } | {
+    type: "UPDATE_PREVIEW_TEXT", payload: {text: string, json: string}
+  }
+) => {
   if (!action || !state) {
-    return {viz: "", curAction: -1};
+    return {viz: "", curAction: -1, englishText: "", prettyJson: ""};
   }
   if (action.type === "UPDATE_VIZ_PREVIEW") {
     return {...state, viz: action.payload};
   }
   if (action.type === "ADVANCE_ACTION") {
-    return {...state, curAction: state.curAction + 1};
+    return {
+      ...state,
+      curAction: state.curAction + 1
+    };
+  }
+  if (action.type === "UPDATE_PREVIEW_TEXT") {
+    return {
+      ...state,
+      englishText: action.payload.text,
+      prettyJson: action.payload.json
+    };
   }
   return {...state};
 };
@@ -34,13 +55,17 @@ const topLevel = combineReducers<{glacier: g.ModelState, display: DispState}>({
 });
 
 const store = createStore(topLevel);
-const actions: g.AllActions[] = [];
+const actions: {action: g.AllActions, english: string}[] = [];
 let lastAction = -1;
 store.subscribe(() => {
   const nextAction = store.getState().display.curAction;
   if (nextAction > lastAction && nextAction < actions.length) {
     lastAction = nextAction;
-    store.dispatch(actions[nextAction]);
+    store.dispatch(actions[nextAction].action);
+    store.dispatch({type: "UPDATE_PREVIEW_TEXT", payload: {
+      text: actions[nextAction].english,
+      json: JSON.stringify(actions[nextAction].action, null, 4)
+    }});
   }
 });
 
@@ -53,48 +78,54 @@ fetch("/data/dji.csv").then(c => c.text()).then(async c => {
     { name: "Low", dataSource: source.id }
   ]);
   actions.push(...[
-    fields,
-    g.createUpdateMarkTypeAction("line"),
-    g.createUpdateDescriptionAction("DJI v Time"),
-    g.createUpdateSizeAction(255, 264),
-    g.createAddChannelAction("x", {
+    {action: fields, english: "Select active fields"},
+    {action: g.createUpdateMarkTypeAction("line"), english: "Set chart type to 'line'"},
+    {action: g.createUpdateDescriptionAction("DJI v Time"), english: "Update description"},
+    {action: g.createUpdateSizeAction(255, 264), english: "Set size"},
+    {action: g.createAddChannelAction("x", {
       field: fields.payload.fields[0].id, type: "temporal", axis: { title: "Date" }
-    }),
-    g.createAddChannelAction("y", {
+    }), english: "Add temporal x axis"},
+    {action: g.createAddChannelAction("y", {
       field: fields.payload.fields[1].id, type: "quantitative", axis: {
         title: "Dow Jones Indstrial Average"
       }, scale: { type: "log" }
-    }),
-    g.createSetFilterAction({type: "GT", left: fields.payload.fields[0], right: `2000-01-01`}),
-    g.createUpdateMarkTypeAction("bar"),
-    g.createUpdateChannelAction("y", {
+    }), english: "Add quantitative y axis"},
+    {
+      action: g.createSetFilterAction({type: "GT", left: fields.payload.fields[0], right: `2000-01-01`}),
+      english: "Filter date to post-2000"
+    },
+    {action: g.createUpdateMarkTypeAction("bar"), english: "Swap chart type to 'bar'"},
+    {action: g.createUpdateChannelAction("y", {
       field: fields.payload.fields[1].id, type: "quantitative", axis: {
         title: "Dow Jones Indstrial Average"
       }
-    }),
-    g.createUpdateChannelAction("x", {
+    }), english: "Remove log scale on y axis"},
+    {action: g.createUpdateChannelAction("x", {
       field: fields.payload.fields[0].id, type: "ordinal", axis: { title: "Date" }
-    }),
-    g.createUpdateSizeAction(255, 528),
-    g.createUpdateChannelAction("y", {
+    }), english: "Make x axis ordinal"},
+    {action: g.createUpdateSizeAction(255, 528), english: "Widen size"},
+    {action: g.createUpdateChannelAction("y", {
       field: fields.payload.fields[1].id, type: "quantitative", axis: {
         title: "Dow Jones Indstrial Average"
       },
       scale: { domain: [8000, 22000], clamp: true }
-    }),
-    g.createUpdateMarkTypeAction("point"),
-    g.createSetFilterAction({type: "GT", left: fields.payload.fields[0], right: `1990-01-01`}),
-    g.createUpdateMarkTypeAction("line"),
-    g.createUpdateChannelAction("y", {
+    }), english: "Adjust y domain and clamp values"},
+    {action: g.createUpdateMarkTypeAction("point"), english: "Change chart type to 'point'"},
+    {
+      action: g.createSetFilterAction({type: "GT", left: fields.payload.fields[0], right: `1990-01-01`}),
+      english: "Change filter to post-1990"
+    },
+    {action: g.createUpdateMarkTypeAction("line"), english: "Change chart type back to 'line'"},
+    {action: g.createUpdateChannelAction("y", {
       field: fields.payload.fields[1].id, type: "quantitative", axis: {
         title: "Dow Jones Indstrial Average"
       },
       scale: { domain: [0, 22000], clamp: true }
-    }),
-    g.createUpdateChannelAction("x", {
+    }), english: "Adjust domain to include 0-8000"},
+    {action: g.createUpdateChannelAction("x", {
       field: fields.payload.fields[0].id, type: "temporal", axis: { title: "Date" }, timeUnit: "year"
-    }),
-    g.createUpdateMarkTypeAction("area")
+    }), english: "Readjust x axis to be temporal and show years for time units"},
+    {action: g.createUpdateMarkTypeAction("area"), english: "Change chart type to 'area'"}
   ]);
   g.createSvgExporter(store, s => s.glacier, async (e) => {
       const data = await e.export();
